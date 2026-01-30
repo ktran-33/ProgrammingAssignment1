4ecf66c3788377c8a28cb29ea10915447f1e3dc1
ktran-33 <kartran309@gmail.com>
2026-01-30 00:33:54 -0500

    verifier part b

--- src/Verifier.py
+++ src/Verifier.py
@@ -12,22 +12,19 @@ def verifier(matches, n, hospital_preferences, student_preferences):
     if n == 0:
         print("VALID STABLE")
         return
-    if matches.empty:
-        print("VALID STABLE")
-        return
-    if hospital_preferences.empty and student_preferences.empty:
-        print("VALID STABLE")
+    if not matches:
+        print("INVALID NO MATCHES")
         return
 
     #(a)  Checks validity: each hospital and each student is matched to exactly one partner, with no duplicates.
     hospital_seen = set()
     student_seen = set()
-    for h, s in matches.items(): # hospital = index, student = value
+    for h, s in enumerate(matches): # hospital = index, student = value
         if h in hospital_seen:
-            print("INVALID VIA HOSPITAL MATCHED MORE THAN ONCE")
+            print("INVALID HOSPITAL MATCHED MORE THAN ONCE")
             return
         if s in student_seen:
-            print("INVALID VIA STUDENT MATCHED MORE THAN ONCE")
+            print("INVALID STUDENT MATCHED MORE THAN ONCE")
             return
         
         # add to the sets
@@ -35,11 +32,53 @@ def verifier(matches, n, hospital_preferences, student_preferences):
         student_seen.add(s)
     
     # Unmatched
-    if len(hospital_seen) != n or len(student_seen):
-        print("INVALID UNMATCHES HOSPITAL AND/OR STUDENT")
+    if len(hospital_seen) != n or len(student_seen) != n:
+        print("INVALID UNMATCHED HOSPITAL AND/OR STUDENT")
         return
 
     #(b) checks stability: confirms there is no blocking pair.
+    # Citation: Algorithm Design Textbook pg 46-47
+
+    # Student Pref Ranking
+    student_rank = []
+    for i in range(n):
+        row = [0] * n
+        student_rank.append(row)
+
+    for s in range(n):
+        for rank, h in enumerate(student_preferences[s]):
+            student_rank[s][h] = rank
+
+    # Hospital Pref Ranking
+    hospital_rank = []
+    for i in range(n):
+        row = [0] * n
+        hospital_rank.append(row)
+
+    for h in range(n):
+        for rank, s in enumerate(hospital_preferences[h]):
+            hospital_rank[h][s] = rank
+
+    # Get Index Student, Value Hospital
+    matches_swap = [0]*n
+    for h, s in enumerate(matches):
+        matches_swap[s] = h
+    
+    # S and H must BOTH prefer each other, but are not matched to each other
+    for h in range(n):
+        s_prime = matches[h]
+
+        for s in range(n):
+            if s_prime == s:
+                continue # Matched with each other already
+
+            h_prime = matches_swap[s]
+            # Compare values student_ranking[s, h] and student_ranking[s, h'] (from TB)
+            # Both prefer each other
+            if (student_rank[s][h] < student_rank[s][h_prime]) and (hospital_rank[h][s] < hospital_rank[h][s_prime]):
+                blocking_pair = "Hospital " + {h+1} + ", Student " + {s+1} # not the current partners
+                print("UNSTABLE Example Blocking Pair: ", blocking_pair)
+                return
         
     print("VALID STABLE")
     return
